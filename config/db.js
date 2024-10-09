bea2e663edb10014e57bd779631761f8d9077edc
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-10-09 19:13:19 +0200

    fix db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
-const { Sequelize } = require('sequelize')
-require('dotenv').config({ path: 'variables.env' })
+const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: 'variables.env' });
 
-// Utilizar DATABASE_URL en lugar de variables individuales
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true, // Esto es importante en Render si utiliza SSL
-            rejectUnauthorized: false // Para evitar problemas con SSL
-        }
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     },
     logging: false
 });
-
-module.exports = sequelize;
