84b9c4037269baf944f3505f0a33987a02e54915
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-10-10 10:44:35 +0200

    fix index

--- config/db.js
+++ config/db.js
@@ -3,16 +3,15 @@ require('dotenv').config({ path: 'variables.env' });
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const sequelize = process.env.DATABASE_URL
-     new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: isProduction ? {
-                require: true,
-                rejectUnauthorized: false,
-            } : false,
-        },
-        logging: false,
-    })
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: isProduction ? {
+            require: true,
+            rejectUnauthorized: false,
+        } : false,
+    },
+    logging: false,
+});
 
 module.exports = sequelize;
